13c37cc26d7008fb3bf989f936d6b7d13b134f51
rtoriasm <arturmelchiori@gmail.com>
2021-11-12 10:18:50 -0300

    getNeighbors fixed and new tests added

---
+++ test/getNeighbors.js
@@ -0,0 +1,126 @@
+const assert = require('assert')
+
+const getNeighbors = require('../src/getNeighbors')
+
+describe('getNeighbors', function() {
+  it('should return true if all neighbors where generated', function() {
+    const solution = [
+      [true,  true,  true],
+      [true, true,  false],
+      [false, false, true]
+    ]
+
+    const neighbors = [
+      [
+        [true,  true,  true],
+        [false, true,  true],
+        [false, false, true]
+      ],
+      [
+        [true,  true,  true],
+        [true, true,  false],
+        [true, false, false]
+      ]
+    ]
+
+    assert(JSON.stringify(getNeighbors(solution)) == JSON.stringify(neighbors))
+  })
+
+  it('should return true if all neighbors where generated', function() {
+    const solution = [
+      [true,  true,  true,  false, true],
+      [false, false, true,  false, true],
+      [true,  false, false, false, true],
+      [false, false, false, false, true],
+      [true,  false, true,  false, true]
+    ]
+
+    const neighbors = [
+      [
+        [true,  true,  true,  false, true],
+        [true,  false, false, true, false],
+        [true,  false, false, false, true],
+        [false, false, false, false, true],
+        [true,  false, true,  false, true]
+      ],
+      [
+        [true,  true,  true,  false,  true],
+        [false, false, true,  false,  true],
+        [true,  false, false, false,  true],
+        [true,  false, false, false, false],
+        [true,  false, true,  false,  true]
+      ]
+    ]
+
+    assert(JSON.stringify(getNeighbors(solution)) == JSON.stringify(neighbors))
+  })
+
+  it('should return true if all neighbors where generated', function() {
+    const solution = [
+      [true,  true,  true,  false, true],
+      [false, false, true,  false, true],
+      [false,  false, false, true, true],
+      [false, false, false, false, true],
+      [true,  false, true,  false, true]
+    ]
+
+    const neighbors = [
+      [
+        [true,  true,  true,  false, true],
+        [true,  false, false, true, false],
+        [false,  false, false, true, true],
+        [false, false, false, false, true],
+        [true,  false, true,  false, true]
+      ],
+      [
+        [true,  true,  true,  false,  true],
+        [false, false, true,  false,  true],
+        [false,  false, false, true,  true],
+        [true,  false, false, false, false],
+        [true,  false, true,  false,  true]
+      ]
+    ]
+
+    assert(JSON.stringify(getNeighbors(solution)) == JSON.stringify(neighbors))
+  })
+
+  it('should return true if all neighbors where generated', function() {
+    const solution = [
+      [true,  false, true,  false, true,  false],
+      [true,  true,  false, true,  true,  false],
+      [false, false, false, true,  true,   true],
+      [true,  false, true,  true,  false, false],
+      [false, true,  true,  true,  true,   true],
+      [true,  true,  true,  false, true,   true]
+    ]
+
+    const neighbors = [
+      [
+        [false,  true, false,  true, false,  true],
+        [true,  true,  false, true,  true,  false],
+        [false, false, false, true,  true,   true],
+        [true,  false, true,  true,  false, false],
+        [false, true,  true,  true,  true,   true],
+        [true,  true,  true,  false, true,   true]
+      ],
+      [
+        [true,  false, true,  false, true,  false],
+        [false, true,  true,  false, true,   true],
+        [false, false, false, true,  true,   true],
+        [true,  false, true,  true,  false, false],
+        [false, true,  true,  true,  true,   true],
+        [true,  true,  true,  false, true,   true]
+      ],
+      [
+        [true,  false, true,  false, true,  false],
+        [true,  true,  false, true,  true,  false],
+        [false, false, false, true,  true,   true],
+        [false, true,  false, true,  true,  false],
+        [false, true,  true,  true,  true,   true],
+        [true,  true,  true,  false, true,   true]
+      ]
+    ]
+
+    assert(JSON.stringify(getNeighbors(solution)) == JSON.stringify(neighbors))
+  })
+})
