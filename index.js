c0e5f47fa78a85017507d557f5a6d2711db23e4d
Lucas Guerreiro Cantarelli <lgcantarelli@gmail.com>
2021-11-17 14:06:16 -0300

    add some temp logs to solveNonogram

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const fitness               = require('./src/fitness')
 const solutionWasNotTested  = require('./src/solutionWasNotTested')
 
 function solveNonogram({ lengths, maxTabuSize, maxIterations, maxIterationsStuck }) {
+  const rowsSize    = lengths.rows.length
+  const columnsSize = lengths.columns.length
+
+  if (rowsSize !== columnsSize)
+    throw new Error(`Rows: ${rowsSize}, Columns: ${columnsSize} dont match!`)
+
   let bestSolution  = generateFirstSolution(lengths)
   let bestCandidate = bestSolution
   let [bestIteration, i] = [0, 0]
@@ -18,6 +24,11 @@ function solveNonogram({ lengths, maxTabuSize, maxIterations, maxIterationsStuck
     const neighborhood = getNeighbors(bestCandidate)
     bestCandidate = neighborhood[0]
 
+    console.log(`i: ${i}`, { bestCandidate })
+    console.log(`i: ${i}`, { fitness: fitness(bestCandidate, lengths) })
+    console.log(`i: ${i}`, 'neighborhood')
+    neighborhood.forEach(solution => console.log(solution))
+
     neighborhood.forEach(candidate => {
       if (solutionWasNotTested(tabuList, candidate) && (fitness(candidate, lengths) < fitness(bestCandidate, lengths)))
         bestCandidate = candidate
@@ -28,6 +39,11 @@ function solveNonogram({ lengths, maxTabuSize, maxIterations, maxIterationsStuck
       bestIteration = i
     }
 
+    console.log(`i: ${i}`, { bestIteration })
+    console.log(`i: ${i}`, { bestSolution })
+    console.log(`i: ${i}`, { fitness: fitness(bestSolution, lengths) })
+    console.log(`i: ${i}`, { bestCandidate })
+
     tabuList.push(bestCandidate)
 
     if (tabuList.length > maxTabuSize)
@@ -37,6 +53,10 @@ function solveNonogram({ lengths, maxTabuSize, maxIterations, maxIterationsStuck
   console.log('FITNESS', fitness(bestSolution, lengths))
 
   return bestSolution
+}
+
+function printNeighborhood(i, neighborhood) {
+
 }
 
 module.exports = solveNonogram
