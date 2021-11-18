8b419089a831a7c45d9d028e12e01c92c08c2d54
Lucas Guerreiro Cantarelli <lgcantarelli@gmail.com>
2021-11-17 21:17:18 -0300

    add a couple more tests

--- index.js
+++ index.js
@@ -24,11 +24,6 @@ function solveNonogram({ lengths, maxTabuSize, maxIterations, maxIterationsStuck
     const neighborhood = getNeighbors(bestCandidate)
     bestCandidate = neighborhood[0]
 
-    // console.log(`i: ${i}`, { bestCandidate })
-    // console.log(`i: ${i}`, { fitness: fitness(bestCandidate, lengths) })
-    // console.log(`i: ${i}`, 'neighborhood')
-    // neighborhood.forEach(solution => console.log(solution))
-
     neighborhood.forEach(candidate => {
       if (solutionWasNotTested(tabuList, candidate) && (fitness(candidate, lengths) < fitness(bestCandidate, lengths)))
         bestCandidate = candidate
@@ -39,24 +34,16 @@ function solveNonogram({ lengths, maxTabuSize, maxIterations, maxIterationsStuck
       bestIteration = i
     }
 
-    // console.log(`i: ${i}`, { bestIteration })
-    // console.log(`i: ${i}`, { bestSolution })
-    // console.log(`i: ${i}`, { fitness: fitness(bestSolution, lengths) })
-    // console.log(`i: ${i}`, { bestCandidate })
-
     tabuList.push(bestCandidate)
 
     if (tabuList.length > maxTabuSize)
       tabuList.shift()
   }
 
+  console.log('i', i)
   console.log('FITNESS', fitness(bestSolution, lengths))
 
   return bestSolution
-}
-
-function printNeighborhood(i, neighborhood) {
-
 }
 
 module.exports = solveNonogram
--- test/solveNonogram.js
+++ test/solveNonogram.js
@@ -87,8 +87,29 @@ describe('solveNonogram', function() {
         maxIterationsStuck: 5
       })
 
-      assert(fitness(bestSolution, lengths) === 0)
-      // assert.deepEqual(bestSolution, solution)
+      assert.deepEqual(bestSolution, solution)
+    })
+
+    it('should return the final solution for a regular 4x4 case', function() {
+      const lengths = {
+        rows:    [3, 4, 1, [1,2]],
+        columns: [[1,1], 2, [2,1], 4]
+      }
+
+      const solution = [
+        [false, true,  true,  true],
+        [true,  true,  true,  true],
+        [false, false, false, true],
+        [true,  false, true,  true]
+      ]
+
+      const bestSolution = solveNonogram({
+        lengths,
+        maxTabuSize: 5,
+        maxIterationsStuck: 20
+      })
+
+      assert.deepEqual(bestSolution, solution)
     })
 
     it('should return the final solution for a simple 5x5 case', function() {
@@ -107,8 +128,8 @@ describe('solveNonogram', function() {
 
       const bestSolution = solveNonogram({
         lengths,
-        maxTabuSize: 10,
-        maxIterationsStuck: 50
+        maxIterationsStuck: 500,
+        maxTabuSize: 50,
       })
 
       assert.deepEqual(bestSolution, solution)
@@ -130,8 +151,8 @@ describe('solveNonogram', function() {
 
       const bestSolution = solveNonogram({
         lengths,
-        maxTabuSize: 10,
-        maxIterationsStuck: 50
+        maxIterationsStuck: 5,
+        maxTabuSize: 5,
       })
 
       assert.deepEqual(bestSolution, solution)
@@ -139,48 +160,70 @@ describe('solveNonogram', function() {
 
     it('should return the final solution for a complex 5x5 case', function() {
       const lengths = {
-        rows:    [[1,1], 5, 1, 3, [2,2]],
-        columns: [[1,1], [1,2], 4, [1,2], [2,1]]
+        rows:    [3, [2,2], 1, [1,1], [1,1]],
+        columns: [[1,1], 2, [1,3], 2, [1,1]]
       }
 
-      const incorrectSolution = [
-        [true,  false, false, true,  false],
-        [true,  true,  true,  true,  true],
-        [false, false, false, false, true],
-        [true, true,  true, false, false],
-        [true,  true,  false,  true, true]
+      const solution = [
+        [ false, true, true, true, false ],
+        [ true, true, false, true, true ],
+        [ false, false, true, false, false ],
+        [ true, false, true, false, false ],
+        [ false, false, true, false, true ]
+      ]
+
+      const bestSolution = solveNonogram({
+        lengths,
+        maxIterationsStuck: 100,
+        maxTabuSize: 20,
+      })
+
+      assert.deepEqual(bestSolution, solution)
+    })
+
+    it('should return the final solution for a regular 6x6 case', function() {
+      const lengths = {
+        rows:    [3, [1,4], [1,3], [1,1], [2,2], 3],
+        columns: [[2,1], [1,4], [2,2], 2, [2,1], 4]
+      }
+
+      const solution = [
+        [true,  true,  true,  false, false, false],
+        [true,  false, true,  true,  true,  true],
+        [false, true, false,  true,  true,  true],
+        [false,  true, false,  false,  false, true],
+        [false,  true, true,  false,  true,  true],
+        [true, true, true, false,  false, false]
       ]
 
       const bestSolution = solveNonogram({
         lengths,
         maxIterationsStuck: 50,
-        maxTabuSize:        50
+        maxTabuSize:        10
       })
 
-      assert.notDeepEqual(bestSolution, incorrectSolution)
-      assert(fitness(incorrectSolution, lengths) > 0)
-      assert(fitness(bestSolution, lengths) === 0)
+      assert.deepEqual(bestSolution, solution)
     })
 
-    it('should return the best solution for a simple 6x6 case', function() {
+    it('should return the final solution for a regular 6x6 case', function() {
       const lengths = {
-        rows:    [3, 4, 4, 3, 3, 1],
-        columns: [2, 2, 5, 4, 4, 1]
+        rows:    [3, [2,1], 4, [4,1], 5, 1],
+        columns: [[2,1], [1,3], 5, 5, 1, [1,2]]
       }
 
       const solution = [
-        [true, true, true, false, false, false],
-        [true, true, true, true, false, false],
-        [false, false, true, true, true, true],
-        [false, false, true, true, true, false],
-        [false, false, true, true, true, false],
-        [false, false, false, false, true, false]
+        [false, true,  true,  true,  false, false],
+        [false, false, true,  true,  false, true],
+        [true,  true,  true,  true,  false, false],
+        [true,  true,  true,  true,  false, true],
+        [false, true,  true,  true,  true,  true],
+        [true,  false, false, false, false, false]
       ]
 
       const bestSolution = solveNonogram({
         lengths,
-        maxIterationsStuck: 500,
-        maxTabuSize:        50
+        maxIterationsStuck: 10,
+        maxTabuSize:        5
       })
 
       assert.deepEqual(bestSolution, solution)
@@ -204,7 +247,7 @@ describe('solveNonogram', function() {
       const bestSolution = solveNonogram({
         lengths,
         maxIterationsStuck: 500,
-        maxTabuSize:        100
+        maxTabuSize:        20
       })
 
       assert.deepEqual(bestSolution, solution)
@@ -212,8 +255,8 @@ describe('solveNonogram', function() {
 
     it('should return the final solution for a complex 6x6 case', function() {
       const lengths = {
-        columns: [[4,1],[2,2],[1,3],[1,1,2],4,[2,1,1]],
-        rows:    [[1,1,1],[3,1],[2,2],[1,1,2],4,6]
+        rows:    [[1,1,1],[3,1],[2,2],[1,1,2],4,6],
+        columns: [[4,1],[2,2],[1,3],[1,1,2],4,[2,1,1]]
       }
 
       const solution = [
